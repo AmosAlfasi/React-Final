01e824ae9edca9e654213de7e6ca2807fff42547
dianaKra <diana13123@gmail.com>
2022-08-11 21:37:05 +0300

    minor fix

--- React-Final-project/src/components/users/UserItem.js
+++ React-Final-project/src/components/users/UserItem.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:React-Final-project/src/components/users/UserItem.js
 import React from 'react';
 import './UserItem.scss';
 
@@ -15,22 +14,4 @@ const UserItem = props => {
     )
 }
 
-=======
-import React from 'react';
-import './UserItem.scss';
-
-const UserItem = props => {
-    const handleClick = (e) => {
-        e.preventDefault();
-        props.onShowInfo(props.id);
-    }
-
-    return (
-        <div onClick={handleClick} className='user'>
-            <span > {props.firstName}  {props.lastName} </span>
-        </div>
-    )
-}
-
->>>>>>> e58f741687887f4fd31fd6fc34734c6426b1803e:React-Final-project/src/components/UserItem.js
 export default UserItem;
